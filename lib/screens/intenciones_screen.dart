5d2de02f4e7c5ed5e8c6fedd4ff7326d64da98cf
Emanuel <emavazquez954@gmail.com>
2024-05-08 01:31:53 -0600

    Consumir api

--- lib/screens/intenciones_screen.dart
+++ lib/screens/intenciones_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class IntencionesScreen extends StatelessWidget {
   const IntencionesScreen({super.key});
@@ -15,27 +16,38 @@ class IntencionesScreen extends StatelessWidget {
           mainAxisSpacing: 8
           ),
         children: [
-          intentCard(),
-          intentCard(),
-          intentCard(),
-          intentCard(),
+          intentCard(icon: Icons.web_stories, title: 'https://itcelaya.edu.mx', sizeLetter: 20, actionFunction: openWeb),
+          intentCard(title: '4111253303', icon: Icons.phone_android_rounded, sizeLetter: 25),
+          intentCard(title: 'sms: 4111253303', icon: Icons.sms, sizeLetter: 20),
+          intentCard(title: 'Email: emavazquez@gmail.com', icon: Icons.email, sizeLetter: 10),
         ],
       ),
     );
   }
 
-  Widget intentCard(){
-    return Container(
-      height: 130,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(40),
-        color: Colors.blueAccent
-      ),
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.ads_click, size: 80,),
-        ],
+  openWeb() async {
+    Uri uri = Uri.parse('https://celaya.tecnm.mx');
+    if(await canLaunchUrl(uri)){
+
+    }
+  }
+
+  Widget intentCard({required String title, required IconData icon, double sizeLetter = 25, Function()? actionFunction}){
+    return GestureDetector(
+      onTap: actionFunction,
+      child: Container(
+        height: 130,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+          color: Colors.blueAccent
+        ),
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 80,),
+            Text(title)
+          ],
+        ),
       ),
     );
   }
---
+++ lib/screens/popular_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:tap_2024/models/popular_models.dart';
+import 'package:tap_2024/network/api_popular.dart';
+
+class PopularScreen extends StatefulWidget {
+  const PopularScreen({super.key});
+
+  @override
+  State<PopularScreen> createState() => _PopularScreenState();
+}
+
+class _PopularScreenState extends State<PopularScreen> {
+
+  ApiPopular? apiPopular;
+
+  @override
+  void initState(){
+    super.initState();
+    apiPopular = ApiPopular();
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Peliculas Populares'),),
+      body: FutureBuilder(
+        future: apiPopular!.getAllPopular(), 
+        builder: (context, AsyncSnapshot<List<PopularModel>?> snapshot) {
+          if(snapshot.hasData){
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) => Text(snapshot.data![index].title),
+            );
+          }else{
+            if(snapshot.hasError){
+              return Text('Ocurrio un error!');
+            }else{
+              return Center(child: CircularProgressIndicator(),);
+            }
+          }
+        },
+      ),
+    );
+  }
+}
